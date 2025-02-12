5bb4e042b61a54cf15057f772a5c270664282d3a
Ather Ali Siddiqui <engr.ather.93@gmail.com>
2025-02-12 12:16:03 +0500

    create login API , specific user API, get all user, also create middleware

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import express from "express";
+import mongoose from "./ConnectDB/dbConnection.js";
+import cors from 'cors'
+import 'dotenv/config'
+import userRouter from "./Routes/UserRoutes.js";
 
-export default function index() {
-  return (
-    <div>
-      Hello World
-    </div>
-  )
-}
+const app = express();
+// const port = 4000;
+const port = process.env.PORT || 4000;  // use process.env.PORT for Vercel
+
+
+// for mongo db connection
+mongoose.connection.on("error", (err) => {
+  console.log("Error in connection", err);
+});
+
+mongoose.connection.on("open", () => {
+  console.log("MongoDB is connected successfully");
+});
+
+
+// main page message
+app.get("/", (req, res) => {
+    res.send("Welcom TO SHARING CAB");
+});
+
+app.use(cors()); // Enable CORS for all routes
+app.use(express.json()); // This will allow us to handle JSON bodies
+
+app.use("/user", userRouter)
+
+app.listen(port, () => {
+  console.log("server is running on port : ", port);
+});
+
